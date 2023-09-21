f598e1661a3fa7cfdf8b29b3c6cf3ab66a2c07f7
Cuitlahuatl <cmorenomagana@gmail.com>
2023-09-20 19:30:33 -0500

    Functions Continuted

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`)
+}
+ const mondayWork = function (routine = "go to the office") {
+    return(`This Monday, I will ${routine}.`)
+}
+function wrapAdjective(special = "*") {
+    return function(innerparamiter = "special") {
+                return(`You are ${special}${innerparamiter}${special}!`)
+    }
+}
